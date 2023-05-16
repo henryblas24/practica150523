150adef9d2fdcad2f2d47f83fa8c7eba393d484c
henry blas <hblas2014@gmail.com>
2023-05-15 22:38:48 -0500

    se realizo tarea de DOM

---
+++ app.js
@@ -0,0 +1,40 @@
+const navigator = document.getElementById("navigator");
+console.log(navigator);
+
+
+const link = document.getElementTagName("a");
+console.log(link);
+
+const link = document.querySelector("navigator nav ul");
+console.log(link);
+
+const link = document.querySelectorAll("navigator nav ul li");
+console.log(link);
+
+const link = document.querySelector('section').SetAttribute('id','section=atribute');
+console.log(link);
+
+
+const firsTli = document.querySelector('ul').firstChild;
+console.log(firsTli);
+
+const firsTli = document.querySelector('li').LastChild;
+console.log(firsTli)
+
+const firsTli = document.querySelector('ul').firsTli.nextSibling;
+console.log(firsTli);
+
+document.querySelector('.content').innerHTML
+document.querySelector('.content').textContent='SOLO TEXTO';
+
+document.querySelector('.content').style='color: red';
+document.querySelector('.content').style='display: none ';
+
+document.querySelector('section').SetAttribute('id','section=atribute');
+document.querySelector.removeAttribute('id');
+ 
+document.querySelector('data-testid','prueba=1');
+document.querySelector.removeAttribute('data-testid');
+
+
+
